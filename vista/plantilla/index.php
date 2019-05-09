a23474ca334dda2f419f3b6b45724b45e6b9b3e0
dr9170 <47762004+dr9170@users.noreply.github.com>
2019-05-09 00:10:49 -0500

    changing

--- vista/plantilla/index.php
+++ vista/plantilla/index.php
@@ -58,7 +58,7 @@
         <section class="text-center bg-white" style="padding-bottom:1rem !important;">
             <div class="container">
                 <img id="logoconfe" class="rounded mx-auto" src="../recursos/images/camaras/confecamaras.jpg" width="231" height="120">
-                <h1 class="jumbotron-heading">Orphan</h1>
+                <h1 class="jumbotron-heading">Changing</h1>
                 <p class="lead text-muted" id="instrucciones1">Seleccione la Cámara a la cual desea Acceder.</p>
             </div>
         </section>
